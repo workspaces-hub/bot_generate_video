228789216f99fc930c427b0bfcf751e2211d5eb3
LinhNT <vanan5923@gmail.com>
2026-07-29 11:52:54 +0700

    fix button

--- src/bot/keyboard.ts
+++ src/bot/keyboard.ts
@@ -1,11 +1,11 @@
 import { Markup } from "telegraf";
 
-export const PROMPT_BUTTON_LABEL = "Prompt";
+export const PROMPT_BUTTON_LABEL = "Video - Start/End Frame";
 export const IMAGE_BUTTON_LABEL = "Image";
 export const VIDEO_REF_BUTTON_LABEL = "Video - Image Reference";
 export const CHARACTER_REF_BUTTON_LABEL = "Video - Character Reference";
 
 export const promptMenu = Markup.keyboard([
-  [PROMPT_BUTTON_LABEL, IMAGE_BUTTON_LABEL],
+  [IMAGE_BUTTON_LABEL, PROMPT_BUTTON_LABEL],
   [VIDEO_REF_BUTTON_LABEL, CHARACTER_REF_BUTTON_LABEL],
 ]).resize();
